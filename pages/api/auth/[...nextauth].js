c7544384287b445eb4cfbd25b9160f7b386faee6
raffii3424235 <raffifariz12@gmail.com>
2023-03-02 20:19:59 +0700

    create api

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+const bcrypt = require("bcryptjs");
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        var sql = "SELECT * FROM users WHERE name = ?";
+        const mysql = require("mysql2/promise");
+        const username = credentials.username;
+        const password = credentials.password;
+
+        const dbs = await mysql.createConnection({
+          host: process.env.DB_HOST,
+          user: process.env.DB_USER,
+          database: process.env.DB_NAME,
+        });
+        const [rows] = await dbs.execute(sql, [username]);
+        const data = rows[0];
+        const user = { id: data.id, name: data.name, role: data.role };
+
+        const checkPassword = bcrypt.compareSync(password, data.password);
+        if (!checkPassword || data.name !== credentials.username) {
+          throw new Error("Username or Password does't match");
+        }
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      var sql = "SELECT * FROM users WHERE name = ?";
+      const mysql = require("mysql2/promise");
+      const username = session.user.name;
+
+      const dbs = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+      });
+      const [rows] = await dbs.execute(sql, [username]);
+      const data = rows[0];
+
+      if (session.user) {
+        session.user.role = data.role;
+      }
+
+      return session;
+    },
+  },
+});
